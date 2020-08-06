f58cb71f8c3d61e4d28b1dd47569139a8de6a3bf
Pysiokot <bszczypka@nano-games.com>
2020-08-06 13:22:54 +0200

    Basic player ship movement

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,19 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    [SerializeField] private Vector2 _movementBoundries;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        var axisInput = Input.GetAxis("Horizontal");
+
+        if (axisInput == 0)
+        {
+            return;
+        }
+
+        var currPlayerPos = this.transform.position;
+
+        currPlayerPos.x = Mathf.Clamp(currPlayerPos.x + (_movementSpeed * axisInput * Time.deltaTime),
+            _movementBoundries.x, _movementBoundries.y);
+
+        this.transform.position = currPlayerPos;
     }
 }
