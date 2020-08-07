357f48c01aaf9b12ea1237848a28354d602cf7b6
Pysiokot <bszczypka@nano-games.com>
2020-08-07 13:05:02 +0200

    Initial class for obstacle

---
+++ Assets/Scripts/SceneManagement/Obstacle.cs
@@ -0,0 +1,32 @@
+﻿using Projectiles;
+using UnityEngine;
+
+namespace SceneManagement
+{
+    public class Obstacle : MonoBehaviour, IProjectileHittable
+    {
+        [SerializeField]
+        private int _hitsToDestroy = 5;
+
+        private float _minAlfa = 0.2f;
+        private float _decreaseStep;
+
+        private void Start()
+        {
+            _decreaseStep = (1f - _minAlfa) / (_hitsToDestroy - 1);
+        }
+
+        public void OnProjectileEnter()
+        {
+            if(_hitsToDestroy == 0)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
+
+            _hitsToDestroy -= 1;
+         
+            // TODO: Shader for destroying object?
+        }
+    }
+}
